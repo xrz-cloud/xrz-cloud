640ddbf7991b884ee460e674041e5fa9eda677c5
xrz-cloud <62951481+xrz-cloud@users.noreply.github.com>
2025-01-23 20:37:42 +0800

    use `Innei/Innei` as profile generator

---
+++ config.ts
@@ -0,0 +1,18 @@
+export const opensource = {
+  active: [
+    "bili-vd-bak/biliroaming-ts-server-vercel",
+    "bili-vd-bak/aniclip",
+    "xrz-cloud/bili-danmaku-xml2any",
+  ],
+  toys: {
+    limit: 3,
+    random: true,
+    repos: ["bili-vd-bak/L1", "xrz-cloud/KernelSU_Action_k305G_picasso_new"],
+  },
+};
+export const motto = `<p align=center><strong>...</strong></p>`;
+export const github = {
+  name: "xrz-cloud",
+};
+
+export const timeZone = "Asia/Shanghai";
